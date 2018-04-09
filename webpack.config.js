1c1f4448c2d66e434e0f515cf95f0e75ada4c945
DanielLotos <daniellika@yandex.ru>
2018-04-09 17:48:17 +0300

    Подготовка настроек

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  mode: isProd
+    ? 'production'
+    : 'development',
+
+  entry: './src/main.js',
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader'
+          ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader',
+            'sass-loader'
+          ]
+        })
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+    new ExtractTextPlugin('style.css')
+  ],
+  devServer: {
+    noInfo: true,
+    overlay: true,
+    open: true,
+    publicPath: '/'
+  }
+};
+
